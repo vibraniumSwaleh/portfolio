608d2cd465c8174240b291f41f2b3b413827b283
vibraniumSwaleh <hus.swaleh@yahoo.com>
2022-06-19 15:07:56 +0100

    Web page responsive and About section started

--- js/script.js
+++ js/script.js
@@ -5,5 +5,13 @@ $(document).ready(function () {
   } else {
    $('.navbar').removeClass("sticky");
   }
- })
+ });
+
+ // toggle menu/navbar script
+ $('.menu-btn').click(function () {
+  $('.navbar .menu').toggleClass("active");
+  $('.menu-btn i').toggleClass("active");
+ });
+  
+
 });
